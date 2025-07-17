09a163049fe80dbeca843511b091ead7c037e7ce
Christian <b.christian.fuentes@gmail.com>
2025-07-17 01:05:15 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Example />
+  </div>
+</template>
+
+<script>
+import Example from './components/example.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Example
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/Example.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'Example',
+    props: [],
+    components: {
+
+    },
+    data() {
+        return {
+            // Define your component's data properties here
+        };
+    },
+    methods: {
+        // Define any methods here
+    },
+    computed: {
+        // Define any computed properties here
+    },
+    mounted() { 
+        // Code to run when the component is mounted
+    },
+}
+</script>
+
+<style scoped></style>
